9a394385999b666f8a04666ee3fd919a14b1839c
Palleti Kiran Kumar Reddy <94693869+Kiran-reddy9998@users.noreply.github.com>
2023-06-27 18:14:45 +0530

    Add files via upload

---
+++ hello_world.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Go Lang")
+}
+
+/*
+The first line package main declares that this file is part of the main package. In Go, the main package is special and serves as the entry point for the executable.
+The second line import "fmt" imports the fmt package, which contains functions for formatting and printing output.
+The func main() is the main function where the program execution begins.
+Inside the main function, fmt.Println("Hello, World!") is used to print the message "Hello, World!" to the standard output. Println is a function from the fmt package that prints a line to the output, and the message is enclosed in double quotes.
+To run this Go program, save it in a file with a .go extension (e.g., hello.go), and then execute the following command in the terminal:
+*/
